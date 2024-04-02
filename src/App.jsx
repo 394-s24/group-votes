89cb76a9d07d33fe9394eb83d02ecbd138372160
hma423 <114116870+hma423@users.noreply.github.com>
2024-04-01 21:15:17 -0500

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
 // V2 OF APP
 
 // React library imports
-import React from 'react';
+import React from "react";
 
 // Components impors
-import PostButton from './components/PostButton';
-import Feed from './components/Feed';
+import PostButton from "./components/PostButton";
+import Feed from "./components/Feed";
 
 // CSS imports (add more after bootstrap)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
 
 // Main App
 const App = () => {
   const today = new Date();
-  const day = today.toLocaleString([], {weekday: 'long'});
-  const date = today.toLocaleDateString([], {dateStyle: 'long'})
+  const day = today.toLocaleString([], { weekday: "long" });
+  const date = today.toLocaleDateString([], { dateStyle: "long" });
 
   return (
-    <div className="container">
+    <div className="container bg-stone-100">
       <h1>GroupVotes</h1>
-      
-      <p>Today is {day}, {date}.</p>
-      
+
+      <p>
+        Today is {day}, {date}.
+      </p>
+
       <Feed groupId="testGroupID" />
 
       <PostButton />
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,51 +1,109 @@
-import React from 'react';
-import './feed.css';
-import useFirebase from '../utilities/firebase';
+import React from "react";
+import "./feed.css";
+import useFirebase from "../utilities/firebase";
 
 const Post = ({ post }) => {
   const { updatePostOptions } = useFirebase(); // Use the useFirebase hook
-  
+
   const handleOptionClick = async (option) => {
     await updatePostOptions("testGroupID", post.id, option); // Call the updatePostOptions function
   };
 
   const renderPostContent = () => {
     switch (post.postType) {
-      case 'Event':
+      case "Event":
         return (
           <>
             <h5>{post.eventName}</h5>
-            {post.eventStart && <p>{post.eventStart} {post.eventEnd}</p>}
+            {post.eventStart && (
+              <p>
+                {post.eventStart} {post.eventEnd}
+              </p>
+            )}
             {post.eventLocation && <p>Location: {post.eventLocation}</p>}
             {post.description && <p>{post.description}</p>}
-            {post.link && <a href={post.link} target="_blank" rel="noopener noreferrer">More Info</a>}
+            {post.link && (
+              <a href={post.link} target="_blank" rel="noopener noreferrer">
+                More Info
+              </a>
+            )}
             {/* Yes, Maybe, No buttons */}
+            <div class="flex items-center justify-start">
+              <button
+                class="bg-green-500 text-white p-2 rounded-lg mr-2 hover:bg-green-300"
+                onClick={() => handleOptionClick("yes")}
+              >
+                Yes
+              </button>
+              <button
+                class="bg-yellow-500 text-white p-2 rounded-lg mr-2 hover:bg-yellow-300"
+                onClick={() => handleOptionClick("maybe")}
+              >
+                {" "}
+                Maybe
+              </button>
+              <button
+                class="bg-red-500 hover:bg-red-400 text-white p-2 rounded-lg "
+                onClick={() => handleOptionClick("no")}
+              >
+                No
+              </button>
+            </div>
+            <div class="flex items-right justify-start mt-4">
+              <p class="text-green-500 mr-2">{post.yes} Yes</p>
+              <p class="text-yellow-500 mr-2">{post.maybe} Maybe</p>
+              <p class="text-red-500">{post.no} No</p>
+            </div>
+
             <div class="flex items-center justify-center">
-  <button class="bg-green-500 text-white p-2 rounded-lg mr-2">Yes</button>
-  <button class="bg-yellow-500 text-white p-2 rounded-lg mr-2">Maybe</button>
-  <button class="bg-red-500 text-white p-2 rounded-lg">No</button>
-</div>
-<div class="flex items-center justify-center mt-4">
-  <p class="text-green-500 mr-2">10 Yes</p>
-  <p class="text-yellow-500 mr-2">5 Maybe</p>
-  <p class="text-red-500">3 No</p>
-</div>
-          </>
-        );
-      case 'Vote':
-        return (
-          <>
-            <h5>{post.question}</h5>
-            {post.link && <a href={post.link} target="_blank" rel="noopener noreferrer">More Info</a>}
-            {/* Yes, Maybe, No buttons */}
-            <div>
-              <button className="btn btn-success mr-2" onClick={() => handleOptionClick('yes')}>Yes</button>
-              <button className="btn btn-secondary mr-2" onClick={() => handleOptionClick('maybe')}>Maybe</button>
-              <button className="btn btn-danger" onClick={() => handleOptionClick('no')}>No</button>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Yes
+              </button>
+              <button class="bg-blue-900 text-white p-1 rounded mr-2">
+                Maybe
+              </button>
+              <button class="bg-blue-900 text-white p-1 rounded">No</button>
+            </div>
+            <div class="flex items-center justify-center mt-4">
+              <p class="mr-2">10 Yes</p>
+              <p class="mr-2">5 Maybe</p>
+              <p>3 No</p>
             </div>
           </>
         );
-      case 'Poll':
+      case "Vote":
+        return (
+          <>
+            <h5>{post.question}</h5>
+            {post.link && (
+              <a href={post.link} target="_blank" rel="noopener noreferrer">
+                More Info
+              </a>
+            )}
+            {/* Yes, Maybe, No buttons */}
+            <div>
+              <button
+                className="btn btn-success mr-2"
+                onClick={() => handleOptionClick("yes")}
+              >
+                Yes
+              </button>
+              <button
+                className="btn btn-secondary mr-2"
+                onClick={() => handleOptionClick("maybe")}
+              >
+                Maybe
+              </button>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleOptionClick("no")}
+              >
+                No
+              </button>
+            </div>
+          </>
+        );
+      case "Poll":
         return (
           <>
             {post.options.map((option, index) => (
@@ -53,11 +111,15 @@ const Post = ({ post }) => {
             ))}
           </>
         );
-      case 'Reminder':
+      case "Reminder":
         return (
           <>
             <h5>{post.text}</h5>
-            {post.link && <a href={post.link} target="_blank" rel="noopener noreferrer">More Info</a>}
+            {post.link && (
+              <a href={post.link} target="_blank" rel="noopener noreferrer">
+                More Info
+              </a>
+            )}
           </>
         );
       default:
@@ -67,7 +129,7 @@ const Post = ({ post }) => {
 
   return (
     <div className="post">
-      <small>"{post.author}"  1hr</small>
+      <small>"{post.author}" 1hr</small>
       {renderPostContent()}
     </div>
   );
