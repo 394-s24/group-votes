1b911914f1f0b33fa9ce1e804b62ef9f4dc61a9a
hma423 <114116870+hma423@users.noreply.github.com>
2024-04-01 21:32:50 -0500

    buttons changed voting and evnet page
    what is dfiference between voting and events?

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,46 +28,34 @@ const Post = ({ post }) => {
               </a>
             )}
             {/* Yes, Maybe, No buttons */}
-            <div class="flex items-center justify-start">
-              <button
-                class="bg-green-500 text-white p-2 rounded-lg mr-2 hover:bg-green-300"
-                onClick={() => handleOptionClick("yes")}
-              >
-                Yes
-              </button>
-              <button
-                class="bg-yellow-500 text-white p-2 rounded-lg mr-2 hover:bg-yellow-300"
-                onClick={() => handleOptionClick("maybe")}
-              >
-                {" "}
-                Maybe
-              </button>
-              <button
-                class="bg-red-500 hover:bg-red-400 text-white p-2 rounded-lg "
-                onClick={() => handleOptionClick("no")}
-              >
-                No
-              </button>
-            </div>
-            <div class="flex items-right justify-start mt-4">
-              <p class="text-green-500 mr-2">{post.yes} Yes</p>
-              <p class="text-yellow-500 mr-2">{post.maybe} Maybe</p>
-              <p class="text-red-500">{post.no} No</p>
-            </div>
-
-            <div class="flex items-center justify-center">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Yes
-              </button>
-              <button class="bg-blue-900 text-white p-1 rounded mr-2">
-                Maybe
-              </button>
-              <button class="bg-blue-900 text-white p-1 rounded">No</button>
-            </div>
-            <div class="flex items-center justify-center mt-4">
-              <p class="mr-2">10 Yes</p>
-              <p class="mr-2">5 Maybe</p>
-              <p>3 No</p>
+            <div class="flex justify-end space-x-4">
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("yes")}
+                >
+                  Yes
+                </button>
+                <p>{post.yes} Yes</p>
+              </div>
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("maybe")}
+                >
+                  Maybe
+                </button>
+                <p>{post.maybe} Maybe</p>
+              </div>
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("no")}
+                >
+                  No
+                </button>
+                <p>{post.no} No</p>
+              </div>
             </div>
           </>
         );
@@ -81,25 +69,34 @@ const Post = ({ post }) => {
               </a>
             )}
             {/* Yes, Maybe, No buttons */}
-            <div>
-              <button
-                className="btn btn-success mr-2"
-                onClick={() => handleOptionClick("yes")}
-              >
-                Yes
-              </button>
-              <button
-                className="btn btn-secondary mr-2"
-                onClick={() => handleOptionClick("maybe")}
-              >
-                Maybe
-              </button>
-              <button
-                className="btn btn-danger"
-                onClick={() => handleOptionClick("no")}
-              >
-                No
-              </button>
+            <div class="flex justify-end space-x-4">
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("yes")}
+                >
+                  Yes
+                </button>
+                <p>{post.yes} Yes</p>
+              </div>
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("maybe")}
+                >
+                  Maybe
+                </button>
+                <p>{post.maybe} Maybe</p>
+              </div>
+              <div class="text-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 text-base rounded"
+                  onClick={() => handleOptionClick("no")}
+                >
+                  No
+                </button>
+                <p>{post.no} No</p>
+              </div>
             </div>
           </>
         );
