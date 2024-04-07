69f7fd0ba21dcef729d9b5dee0631d98b6708310
hma423 <114116870+hma423@users.noreply.github.com>
2024-04-07 17:45:19 -0500

    added escpe and click off for the post button

--- src/components/PostButton.jsx
+++ src/components/PostButton.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import Modal from './Modal'; // Modal component
-import NewPost from './NewPost'; // NewPost component
+import { useEffect, useState } from "react";
+import Modal from "./Modal"; // Modal component
+import NewPost from "./NewPost"; // NewPost component
 
 const PostButton = () => {
   const [open, setOpen] = useState(false);
@@ -8,11 +8,40 @@ const PostButton = () => {
   const openModal = () => setOpen(true);
   const closeModal = () => setOpen(false);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      // Ensure the modal is open and the click is not on the modal trigger button
+      if (open && !event.target.closest(".btn-primary")) {
+        closeModal();
+      }
+    };
+
+    // Effect for handling the Escape key press
+    const handleEscapePress = (event) => {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    };
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleEscapePress);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapePress);
+    };
+  }, [open]);
   return (
     <div>
       {/* Button to trigger modal for new post creation */}
-      {!open && <button className="btn btn-primary" onClick={openModal}>+ Post</button>}
-      
+      {!open && (
+        <button className="btn btn-primary" onClick={openModal}>
+          + Post
+        </button>
+      )}
+
       {/* Modal dialog for NewPost component */}
       <Modal open={open} close={closeModal}>
         <NewPost groupId="testGroupID" userID="testUserID" />
