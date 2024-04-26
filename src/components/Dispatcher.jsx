7814f89545d1e5c4a6efc6b05076f5c41fb7e587
hyunbinjlee <hyunbinlee@u.northwestern.edu>
2024-04-26 16:58:05 -0500

    display groupsPage with dummy text, have to link to firebase

--- src/components/Dispatcher.jsx
+++ src/components/Dispatcher.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 import Feed from './Feed';
+import GroupsPage from './GroupsPage';
 
 const Dispatcher = () => (
   <BrowserRouter>
   
     <Routes>
       <Route path="/" element={<Feed groupId={"testGroupID"} />} />
-      {/* <Route path="/group" element={<Group users={users} />} /> */}
+      <Route path="/group" element={<GroupsPage/>} />
     </Routes>
     
     <div style={{ position: "fixed", bottom: "0px", left: "0", right: "0", maxWidth: "100vw" }}>
